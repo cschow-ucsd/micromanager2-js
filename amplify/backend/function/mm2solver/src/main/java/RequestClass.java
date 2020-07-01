6835a897d50a12094490ed3ba67e429157fbd7f1
Chi Chow <kyroschow@gmail.com>
2020-06-30 17:35:25 -0700

    Add Solver Lambda to Amplify

---
+++ amplify/backend/function/mm2solver/src/main/java/RequestClass.java
@@ -0,0 +1,36 @@
+/* Amplify Params - DO NOT EDIT
+	API_MICROMANAGER2API_GRAPHQLAPIIDOUTPUT
+	API_MICROMANAGER2API_MMSCHEDULETABLE_ARN
+	API_MICROMANAGER2API_MMSCHEDULETABLE_NAME
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+        
+     public class RequestClass {
+        String firstName;
+        String lastName;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public RequestClass(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public RequestClass() {
+        }
+    }
---
+++ amplify/backend/function/mm2solver/src/main/java/ResponseClass.java
@@ -0,0 +1,23 @@
+/* Amplify Params - DO NOT EDIT
+	API_MICROMANAGER2API_GRAPHQLAPIIDOUTPUT
+	API_MICROMANAGER2API_MMSCHEDULETABLE_ARN
+	API_MICROMANAGER2API_MMSCHEDULETABLE_NAME
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+        
+     public class ResponseClass {
+        String greetings;
+
+        public String getGreetings() {
+            return this.greetings;
+        }
+
+        public void setGreetings(String greetings) {
+            this.greetings = greetings;
+        }
+
+        public ResponseClass(String greetings) {
+            this.greetings = greetings;
+        }
+    }
